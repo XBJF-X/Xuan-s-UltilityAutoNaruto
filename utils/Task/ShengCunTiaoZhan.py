13b3fa6114ebba9f1e6d853914befefa35f33bc0
玄不救非 <3034160006@qq.com>
2025-08-09 10:12:26 +0800

    fix: 修复生存挑战bug
    修复生存挑战通过但未重置时任务卡死

--- utils/Task/ShengCunTiaoZhan.py
+++ utils/Task/ShengCunTiaoZhan.py
@@ -39,21 +39,35 @@ class ShengCunTiaoZhan(BaseTask):
         self.click_and_wait({"type": "COORDINATE", "coordinate": [800, 750]})
         self.logger.info("开始扫荡")
         # 点击开始扫荡图标
-        self.click_and_wait({'type': "ELEMENT", 'name': "生存挑战-开始扫荡"})
-        if self.detect_and_wait(
-            {'type': "ELEMENT", 'name': "生存挑战-没有可以出战的忍者"},
-            max_time=2,
-            auto_raise=False
-        ):
-            self.logger.warning("没有可出战的忍者，将进行重置")
+        self.click_and_wait(
+            {'type': "ELEMENT", 'name': "生存挑战-开始扫荡"},
+            wait_time=0
+        )
+        flag = self.search_and_detect(
+            [
+                {'type': "ELEMENT", 'name': "生存挑战-没有可以出战的忍者"},
+                {'type': "ELEMENT", 'name': "生存挑战-已通过所有关卡"}
+            ],
+            [],
+            search_max_time=2,
+            once_max_time=0.5
+        )
+        if flag:
+            if flag == 1:
+                self.logger.warning("没有可出战的忍者，将进行重置")
+            elif flag == 2:
+                self.logger.warning("已通过所有关卡，将进行重置")
             if self.click_and_wait(
-                {'type': "ELEMENT", 'name': "生存挑战-重置"},
-                auto_raise=False
+                    {'type': "ELEMENT", 'name': "生存挑战-重置"},
+                    auto_raise=False
             ):
                 self.click_and_wait({'type': "ELEMENT", 'name': "生存挑战-重置-确认"})
                 # 随便点一下把自动打的弹窗点掉
                 self.click_and_wait({"type": "COORDINATE", "coordinate": [800, 750]})
                 self.click_and_wait({"type": "COORDINATE", "coordinate": [800, 750]})
+                self.logger.info("重置后再次开始扫荡")
+                # 点击开始扫荡图标
+                self.click_and_wait({'type': "ELEMENT", 'name': "生存挑战-开始扫荡"})
 
         # 点击准备就绪
         self.click_and_wait({'type': "ELEMENT", 'name': "生存挑战-准备就绪"})
