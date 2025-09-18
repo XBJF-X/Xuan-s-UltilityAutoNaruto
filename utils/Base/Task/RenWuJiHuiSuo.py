d130449b79a09e1c5f42feebe29eb321f7bc8663
玄不救非 <3034160006@qq.com>
2025-09-18 15:25:57 +0800

    fix: 修复任务集会所两个场景中搜索不存在的元素

--- utils/Base/Task/RenWuJiHuiSuo.py
+++ utils/Base/Task/RenWuJiHuiSuo.py
@@ -16,42 +16,22 @@ class RenWuJiHuiSuo(BaseTask):
     @TransitionOn()
     def _(self):
         self.logger.info("开始领取任务奖励")
-        if self.operationer.click_and_wait(
-                "可领取",
-                auto_raise=False
-        ):
+        if self.operationer.click_and_wait("可领取"):
             return False
 
         self.logger.info("开始接取任务")
         # 先看看是不是已经领完了所有任务了
-        if self.operationer.detect_element(
-                "今天所有任务已经领完",
-                auto_raise=False
-        ):
+        if self.operationer.detect_element("今天所有任务已经领完"):
             self.update_next_execute_time()
             raise EndEarly("今日任务已经领完，提前退出执行")
 
-        while not self.operationer.search_and_detect(
-                [
-                    self.operationer.get_element("任务栏满了"),
-                    self.operationer.get_element("今天所有任务已经领完"),
-                ],
-                [],
-                once_max_time=0.5,
-                search_max_time=1,
-                bool_debug=True
+        if not self.operationer.detect_element(
+                "今天所有任务已经领完",
+                max_time=0.5,
         ):
-            if self.operationer.click_and_wait(
-                    "接取",
-                    max_time=0.3,
-                    auto_raise=False
-            ):
+            if self.operationer.click_and_wait("接取"):
                 return False
-            if self.operationer.click_and_wait(
-                    "超影免费",
-                    max_time=0.3,
-                    auto_raise=False
-            ):
+            if self.operationer.click_and_wait("超影免费"):
                 self.logger.info("刷新任务列表")
             else:
                 # 能接取的都接了，无法刷新，可以退出执行了
@@ -79,27 +59,16 @@ class RenWuJiHuiSuo(BaseTask):
             "出发",
             wait_time=0
         )
-        # 检查任务栏是否已满
-        match self.operationer.search_and_detect(
-            [
-                self.operationer.get_element("任务栏满了"),
-                self.operationer.get_element("今天所有任务已经领完"),
-            ],
-            [],
-            once_max_time=0.5,
-            search_max_time=1
+        if self.operationer.detect_element(
+            "任务栏满了",
+            max_time=0.7
         ):
-            case 0:
-                self.task_sum += 1
-                self.logger.info(f"已接取 {self.task_sum} 个任务")
-            case 1:
-                self.operationer.click_and_wait("X")
-                self.update_next_execute_time(3, timedelta(hours=1))
-                raise EndEarly("任务栏已满，等待下次检查")
-            case 2:
-                self.operationer.click_and_wait("X")
-                self.update_next_execute_time()
-                raise EndEarly("任务栏已满/今日任务已经领完")
+            self.operationer.click_and_wait("X")
+            self.update_next_execute_time(3, timedelta(hours=1))
+            raise EndEarly("任务栏已满，等待下次检查")
+        else:
+            self.task_sum += 1
+            self.logger.info(f"已接取 {self.task_sum} 个任务")
         return False
 
     @TransitionOn("任务集会所-一键领取")
