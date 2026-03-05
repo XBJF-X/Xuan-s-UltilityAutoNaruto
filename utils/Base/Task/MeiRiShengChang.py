d66a1efdf023c108749b8b29a09d747bb77e3b4f
玄不救非 <3034160006@qq.com>
2026-03-05 16:27:18 +0800

    fix: 修复[每日胜场]如果补签卡数量不足陷入跳转循环

--- utils/Base/Task/MeiRiShengChang.py
+++ utils/Base/Task/MeiRiShengChang.py
@@ -46,6 +46,7 @@ class MeiRiShengChang(BaseTask):
     def _(self):
         self.operationer.clicker.stop()
         self.logger.info("领取所有待领取的决斗任务宝箱")
+
         while self.operationer.search_and_click(
                 [
                     "宝箱-领取"
@@ -54,11 +55,7 @@ class MeiRiShengChang(BaseTask):
                 max_attempts=1,
         ):
             continue
-        # 检测有无可追回宝箱
-        if self.operationer.click_and_wait("宝箱-追回"):
-            self.logger.info("存在可追回每日胜场宝箱")
-            self.checked = True
-            return False
+
         if not self.operationer.search_and_detect(
                 [self.operationer.get_element("宝箱-未达成")],
                 [
@@ -69,6 +66,16 @@ class MeiRiShengChang(BaseTask):
                 max_attempts=1,
                 bool_debug=True
         ):
+            self.operationer.swipe_and_wait(
+                (1095, 167),
+                (1095, 618),
+                duration=0.7)
+            # 检测有无可追回宝箱
+            if self.operationer.click_and_wait("宝箱-追回"):
+                self.logger.info("存在可追回每日胜场宝箱")
+                self.checked = True
+                return False
+
             self.checked = False
             self.operationer.click_and_wait("X")
             self.logger.info("结束执行")
@@ -83,7 +90,11 @@ class MeiRiShengChang(BaseTask):
     def _(self):
         self.operationer.click_and_wait("追回", wait_time=0)
         if self.operationer.detect_element("道具不足", auto_raise=False):
+            self.checked = False
             self.operationer.click_and_wait("X")
+            self.logger.info("结束执行")
+            self.update_next_execute_time()
+            return True
         return False
 
     @TransitionOn("决斗场-结算")
