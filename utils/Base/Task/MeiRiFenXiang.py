61a6a3badaad16a6de887d695b9e8da2e08e4d63
玄不救非 <3034160006@qq.com>
2026-04-03 20:38:13 +0800

    perf: 优化分享逻辑

--- utils/Base/Task/MeiRiFenXiang.py
+++ utils/Base/Task/MeiRiFenXiang.py
@@ -17,19 +17,20 @@ class MeiRiFenXiang(BaseTask):
         start_time = time.perf_counter()
         while self.operationer.is_naruto_frontend:
             self.logger.debug("火影忍者仍处于前台...")
-            time.sleep(1)
+            time.sleep(2)
             if time.perf_counter() - start_time > timeout:
                 self.logger.debug("跳转分享失败，未能跳出游戏，请检查是否安装QQ/微信(与游戏账号对应)")
                 raise
         start_time = time.perf_counter()
         while not self.operationer.is_naruto_frontend:
             self.logger.debug("跳转成功，将返回游戏...")
-            time.sleep(1)
+            time.sleep(2)
             self.operationer.app_start()
-            time.sleep(1)
+            time.sleep(2)
             if not self.operationer.is_naruto_frontend:
                 self.logger.debug("返回游戏失败，将尝试点击[Esc]键")
                 self.operationer.press_key("BACK")
+                time.sleep(1)
 
             if time.perf_counter() - start_time > timeout:
                 self.logger.debug("返回游戏失败，请自行检查...")
