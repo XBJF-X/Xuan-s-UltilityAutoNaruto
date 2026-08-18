00fc7da9f906fb1d098bb4240b9e62c45bead386
玄不救非 <3034160006@qq.com>
2026-08-18 22:58:59 +0800

    fix:[商城奖励]修复无寻路导致的卡死

--- backend/core/legacy/Scene/TransitionManager.py
+++ backend/core/legacy/Scene/TransitionManager.py
@@ -704,7 +704,17 @@ class TransitionManager:
 
         @self.register("商城", "商城-商店")
         def _(operationer: Operationer, *args, **kwargs):
-            operationer.click_and_wait("商店")
+            operationer.search_and_click(
+                    ["商店"],
+                    [{
+                        "swipe": {
+                            "start_coordinate": [137, 733],
+                            "end_coordinate": [137, 224],
+                            "duration": 0.5
+                        }
+                    }],
+                    max_attempts=3,
+            )   
 
         @self.register("登录界面", "主场景")
         def _(operationer: Operationer, *args, **kwargs):
