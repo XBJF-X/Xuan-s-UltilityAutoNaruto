c7069b91f35315de222e86d6b3b4d8e08c11e017
玄不救非 <3034160006@qq.com>
2026-08-23 13:35:38 +0800

    perf:[金币招财]使用Ocr优化已招财进度获取，不再依赖配置文件中的进度

--- backend/core/legacy/Task/JinBiZhaoCai.py
+++ backend/core/legacy/Task/JinBiZhaoCai.py
@@ -1,4 +1,5 @@
 from datetime import timedelta
+import re
 
 from backend.core.legacy.Exceptions import TaskCompleted, StepFailedError, TaskCompleted
 from backend.core.legacy.Task.BaseTask import BaseTask, TransitionOn
@@ -8,28 +9,37 @@ class JinBiZhaoCai(BaseTask):
     source_scene = "招财"
     task_max_duration = timedelta(minutes=3)
 
+    def run(self):
+        self.target_times=self.config.get_task_exe_param("金币招财", "招财次数")
+        self.process_times=0
+        return super().run()
+
     @TransitionOn()
     def _(self):
-        if (self.config.get_task_exe_prog("金币招财", "已招财次数") >=
-                self.config.get_task_exe_param("金币招财", "招财次数", 2)):
+        yzccs=self.operationer.ocr_recognize("已招财次数")
+        if yzccs:
+            pattern = re.compile(
+                r'第\s*(\d+)\s*/\s*12\s*轮\s*[，,]\s*累积投币\s*[:：]\s*(\d+)\s*/\s*10'
+            )
+            match = pattern.search(yzccs[0].text)
+            if match:
+                x,y=int(match.group(1)), int(match.group(2))
+                self.process_times=10*(x-1)+y
+                self.logger.info(f"识别到当前已金币招财 {self.process_times} 次")
+
+        if self.process_times>=self.target_times:
             raise TaskCompleted("已招满金币招财")
 
-        if self.operationer.click_and_wait("免费一次", wait_time=2):
-            self.logger.info("免费招财一次")
-            times = self.config.get_task_exe_prog("金币招财", "已招财次数")
-            times += 1
-            self.config.set_task_exe_prog("金币招财", "已招财次数", times)
+        if self.process_times < self.target_times:
+            self.operationer.click_and_wait("金币招财")
+            self.process_times += 1
+            self.logger.info(f"已招财 {self.process_times} 次")
             return False
 
-        times = self.config.get_task_exe_prog("金币招财", "已招财次数")
-        if times < self.config.get_task_exe_param("金币招财", "招财次数"):
-            self.operationer.click_and_wait("金币招财")
-            times += 1
-            self.config.set_task_exe_prog("金币招财", "已招财次数", times)
-            self.logger.info(f"已招财 {times} 次")
-            return False
         self.operationer.click_and_wait("X")
         raise TaskCompleted("任务执行完成")
+    
+
     @TransitionOn("二级密码")
     def _(self):
         self.logger.debug("出现二级密码窗口")
@@ -44,10 +54,9 @@ class JinBiZhaoCai(BaseTask):
         # 点击二级密码-确定
         if not self.operationer.click_and_wait(self.operationer.get_element("确定")):
             raise StepFailedError("二级密码验证失败")
-        times = self.config.get_task_exe_prog("金币招财", "已招财次数")
-        times -= 1
-        self.config.set_task_exe_prog("金币招财", "已招财次数", times)
-        self.logger.info(f"招财次数回退，已招财 {times} 次")
+
+        self.process_times -= 1
+        self.logger.info(f"招财次数回退，已招财 {self.process_times} 次")
         return False
 
     def reset_task_exe_prog(self) -> bool:
