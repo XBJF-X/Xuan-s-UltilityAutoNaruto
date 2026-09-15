e9519c2181ca233e817b4d81b0599e1e4ee56397
玄不救非 <3034160006@qq.com>
2026-09-15 19:56:05 +0800

    perf:[活跃度奖励]使用Ocr优化奖励领取逻辑

--- backend/core/legacy/Task/HuoYueDuJiangLi.py
+++ backend/core/legacy/Task/HuoYueDuJiangLi.py
@@ -21,22 +21,23 @@ class HuoYueDuJiangLi(BaseTask):
             else:
                 self.schedule_next_with_delay(timedelta(hours=3))
                 raise TaskCompleted("活跃度不足，延迟重试")
+            
+        hyd=self.operationer.ocr_recognize("活跃度")
+        hyd_num=hyd.get_first_number() or 100
 
-        if not self.config.get_task_exe_prog(self.task_name, f"10活跃度已领取", False):
-            self._handle_daily_activity_reward(10)
-            return False
-        elif not self.config.get_task_exe_prog(self.task_name, f"40活跃度已领取", False):
-            self._handle_daily_activity_reward(40)
-            return False
-        elif not self.config.get_task_exe_prog(self.task_name, f"80活跃度已领取", False):
-            self._handle_daily_activity_reward(80)
-            return False
-        elif not self.config.get_task_exe_prog(self.task_name, f"100活跃度已领取", False):
-            self._handle_daily_activity_reward(100)
-            return False
-        elif not self.config.get_task_exe_prog(self.task_name, f"周活跃度已领取", False):
+        TIERS=[10,40,80,100]
+
+        filter_tiers=[x for x in TIERS if x <= hyd_num]
+        
+        for tier in filter_tiers:
+            self.operationer.click_and_wait(f"每日活跃度-{tier}-待领取",wait_time=1,max_time=1.5,click_times=2,click_interval=1)
+            if self.operationer.detect_element(f"每日活跃度-{tier}-已领取",wait_time=0, max_time=1):
+                self.config.set_task_exe_prog(self.task_name, f"{tier}活跃度已领取", True)
+
+        if not self.config.get_task_exe_prog(self.task_name, f"周活跃礼已领取", False):
             self.operationer.click_and_wait("周活跃礼")
             return False
+        
         self.finished = True
         return False
 
@@ -49,57 +50,8 @@ class HuoYueDuJiangLi(BaseTask):
         self.finished = True
         return False
 
-    def _handle_daily_activity_reward(self, num):
-        # 点击待领取的宝箱
-        self.logger.info(f"[{num}活跃度宝箱] 开始领取")
-        if not self.operationer.detect_element(
-                f"每日活跃度-{num}-已领取",
-                wait_time=0,
-                max_time=0.3
-        ):
-            if self.operationer.detect_element(
-                    f"每日活跃度-{num}-待领取",
-                    wait_time=1,
-                    max_time=1
-            ):
-                try_times = 0
-                # 检测到可以领取，则一直领取直到已领取的状态
-                while not self.operationer.detect_element(
-                        f"每日活跃度-{num}-已领取",
-                        wait_time=0,
-                        max_time=0.3
-                ):
-                    self.operationer.click_and_wait(
-                        f"每日活跃度-{num}-待领取",
-                        wait_time=1,
-                        max_time=1
-                    )
-                    try_times += 1
-                    if try_times >= 3:
-                        self.logger.warning(f"{num}活跃度宝箱领取超过3次失败，将不再尝试")
-                        return
-                self.config.set_task_exe_prog(self.task_name, f"{num}活跃度已领取", True)
-                self.logger.info(f"[{num}活跃度宝箱] 领取成功")
+        
 
-            else:
-                # 如果没有检测到可以领取，则返回且报警告
-                self.logger.info(f"[{num}活跃度宝箱] 活跃度不足")
-                self.finished = True
-            # if not self.operationer.click_and_wait(
-            #         f"每日活跃度-{num}-待领取",
-            #         wait_time=1,
-            #         max_time=1,
-            #         auto_raise=False
-            # ):
-            #     self.logger.info(f"[{num}活跃度宝箱] 活跃度不足")
-            #     self.finished = True
-            #     return
-            # else:
-            #     self.config.set_task_exe_prog(self.task_name, f"{num}活跃度已领取", True)
-            #     self.logger.info(f"[{num}活跃度宝箱] 领取成功")
-        else:
-            self.config.set_task_exe_prog(self.task_name, f"{num}活跃度已领取", True)
-            self.logger.info(f"[{num}活跃度宝箱] 已领取")
 
     def reset_task_exe_prog(self) -> bool:
         self.finished = False
