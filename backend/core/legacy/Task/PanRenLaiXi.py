8dabad8556efe8ef3007b3277c54a85ebbcb6b46
玄不救非 <3034160006@qq.com>
2026-09-16 23:25:29 +0800

    bump: version 0.17.56 → 0.17.57

--- backend/core/legacy/Task/PanRenLaiXi.py
+++ backend/core/legacy/Task/PanRenLaiXi.py
@@ -86,6 +86,7 @@ class PanRenLaiXi(BaseTask):
         if self.config.get_task_exe_param(self.task_name, "是否需要开启叛忍", True):
             self.operationer.click_and_wait("开启")
         self.operationer.click_and_wait("X")
+        self.operationer.next_scene = "主场景-组织"
         return False
 
     @TransitionOn("叛忍来袭-即将开始")
